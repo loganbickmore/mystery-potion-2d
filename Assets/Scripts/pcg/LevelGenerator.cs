2ca97eb0ce2a10d0205b79605b9e7139fb58043b
Logan Bickmore <loganbickmore91@gmail.com>
2020-07-11 10:52:59 -0700

    refactored map gen and added some debugging

--- Assets/Scripts/pcg/LevelGenerator.cs
+++ Assets/Scripts/pcg/LevelGenerator.cs
@@ -5,7 +5,7 @@ using UnityEngine.Tilemaps;
 public class LevelGenerator : MonoBehaviour
 {
     // public Tilemap tilemap;
-    public TileBase tile;
+    // public TileBase tile;
     public int width;
     public int height;
     public float seed;
@@ -19,32 +19,38 @@ public class LevelGenerator : MonoBehaviour
 
     void Start()
     {
-        // GenerateMap();
+        grid = new GameObject("Grid_0").AddComponent<Grid>();
 
-        if (randomSeed) {
-            seed = Time.time;
-        }
-        
-        grid = new GameObject("Grid0").AddComponent<Grid>();
-
-        // clear tiles
         for (int i = 0 ; i <= tiles.GetUpperBound(0); i++) {
-            var go = new GameObject("Tilemap"+i);
-            var tm = go.AddComponent<Tilemap>();
-            var tr = go.AddComponent<TilemapRenderer>();
-
-            tm.tileAnchor = new Vector3(0,0,0);
-            go.transform.SetParent(grid.transform);
-            tr.sortingLayerName = "Main";
-
-            tm.ClearAllTiles();
-            GenerateMap(tm, tiles[i]);
-
+            var tilemap = CreateTilemap("Tilemap_"+i);
+            tilemap.ClearAllTiles();
+            GenerateMap(tilemap, tiles[i]);
         }
+    }
 
-        
+    private Tilemap CreateTilemap(string name)
+    {
+            var gameObject = new GameObject(name);
+            var tilemap = gameObject.AddComponent<Tilemap>();
+            var tilemapRenderer = gameObject.AddComponent<TilemapRenderer>();
 
+            tilemap.tileAnchor = new Vector3(0,0,0);
+            gameObject.transform.SetParent(grid.transform);
+            tilemapRenderer.sortingLayerName = "Main";
 
+            return tilemap;
+    }
+
+    private string LogMap(int[,] map)
+    {
+        var str = "";
+        for (int y = map.GetUpperBound(1); y > 0; y--) {
+            for (int x = 0; x < map.GetUpperBound(0); x++) {
+                str = str + map[x,y] + ", ";
+            }
+            str = str + "\n";
+        }
+        return str;
     }
 
     public void GenerateMap(Tilemap tilemap, TileBase tile)
@@ -57,7 +63,10 @@ public class LevelGenerator : MonoBehaviour
 
         int[,] map = new int[width, height];
         map = MapHelpers.GenerateArray(width, height, true);
+        Debug.Log("GenerateArray:\n" + LogMap(map));
+
         map = MapHelpers.PerlinNoise(map, seed);
+        Debug.Log("PerlinNoise:\n" + LogMap(map));
         MapHelpers.RenderMap(map, tilemap, tile);
     }
 }
